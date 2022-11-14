6a5187fc9279096928a948792df76526b2fdddba
Dani <24252795+SGD14@users.noreply.github.com>
2022-11-14 14:05:23 +0100

    Add win check logic

--- javascript.js
+++ javascript.js
@@ -16,20 +16,36 @@ const game = (() => {
     const player2 = createPlayer("Player 2", "X");
 
     let currentPlayer = player1;
+    let winner = undefined;
 
     const addNextMark = (position) => {
-        if(board[position])
+        if(winner || board[position])
             return;
 
         board[position] = currentPlayer;
 
         currentPlayer = currentPlayer === player1 ? player2 : player1;
+
+        checkWinner();
     }
 
-    return {board, player1, player2, currentPlayer, addNextMark};
+    const checkWinner = () => {
+        const cellGroups = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+        console.log("Checking winner")
+
+        const winGroup = cellGroups.find(cellGroup => board[cellGroup[0]] && board[cellGroup[0]] === board[cellGroup[1]] && board[cellGroup[1]] === board[cellGroup[2]])
+
+        console.log(winGroup);
+
+        if(winGroup)
+            winner = board[winGroup[0]];
+    }
+
+    return {board, player1, player2, currentPlayer, addNextMark, winner};
 })();
 
-const renderBoard = () => {
+const renderGame = () => {
     const boardCells = document.querySelectorAll(".board > div");
 
     boardCells.forEach((cell, index) => {
@@ -42,6 +58,6 @@ const renderBoard = () => {
 document.querySelectorAll(".board > div").forEach((cell, index) => {
     cell.addEventListener('click', (event) => {
         game.addNextMark(index);
-        renderBoard();
+        renderGame();
     });
 });
