cd5aeff6f1aeea9b3f954aeedfce062e4e8c71ae
Dani <24252795+SGD14@users.noreply.github.com>
2022-11-14 16:42:55 +0100

    Add player name change option and game info text

--- javascript.js
+++ javascript.js
@@ -1,19 +1,28 @@
 const game = (() => {
-    const board = [];
+    let board = [];
+
+    const getBoard = () => board;
 
     const createPlayer = (playerName, playerSymbol) => {
         let name = playerName;
+        const getName = () => name;
+        const setName = (newName) => name = newName;
         const symbol = playerSymbol;
 
-        return {name, symbol};
+        return {getName, setName, symbol};
     }
 
     const player1 = createPlayer("Player 1", "O");
     const player2 = createPlayer("Player 2", "X");
 
     let currentPlayer = player1;
+    const getCurrentPlayer = () => currentPlayer;
+
     let winner = undefined;
+    const getWinner = () => winner;
+
     let gameState = "playing";
+    const getGameState = () => gameState;
 
     const addNextMark = (position) => {
         if(gameState !== "playing" || board[position])
@@ -41,18 +50,37 @@ const game = (() => {
             gameState = "ended";
     }
 
-    return {board, player1, player2, currentPlayer, addNextMark, winner};
+    const reset = () => {
+        board = [];
+        currentPlayer = player1;
+        winner = undefined;
+        gameState = "playing";
+    }
+
+    return {getBoard, player1, player2, getCurrentPlayer, getGameState, addNextMark, getWinner, reset};
 })();
 
 const renderGame = () => {
     const boardCells = document.querySelectorAll(".board > div");
 
     boardCells.forEach((cell, index) => {
-        const cellPlayer = game.board[index];
+        const cellPlayer = game.getBoard()[index];
 
         cell.innerHTML = cellPlayer ? cellPlayer.symbol : '';
-    })
+    });
+
+    if(game.getGameState() === "ended") {
+        if(game.getWinner())
+            document.querySelector("#info-text").innerHTML = "The winner is " + game.getWinner().getName() + "!";
+        else
+            document.querySelector("#info-text").innerHTML = "The game ended in tie!";
+    } else {
+        document.querySelector("#info-text").innerHTML = "";
+    }
 }
+
+document.querySelector("#player-1-name").value = "Player 1";
+document.querySelector("#player-2-name").value = "Player 2";
 
 document.querySelectorAll(".board > div").forEach((cell, index) => {
     cell.addEventListener('click', (event) => {
@@ -60,3 +88,18 @@ document.querySelectorAll(".board > div").forEach((cell, index) => {
         renderGame();
     });
 });
+
+document.querySelector("#reset-button").addEventListener("click", event => {
+    game.reset();
+    renderGame();
+});
+
+document.querySelector("#player-1-name").addEventListener("input", event => {
+    game.player1.setName(event.target.value);
+    renderGame();
+})
+
+document.querySelector("#player-2-name").addEventListener("input", event => {
+    game.player2.setName(event.target.value);
+    renderGame();
+})
